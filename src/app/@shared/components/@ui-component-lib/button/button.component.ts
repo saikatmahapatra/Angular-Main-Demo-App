87d3007461e9ac4284f4e9d93eb63ed95be30cea
Saikat Mahapatra <saikat.mahapatra@accenture.com>
2025-11-21 18:20:15 +0530

    create button UI

--- src/app/@shared/components/@ui-component-lib/button/button.component.ts
+++ src/app/@shared/components/@ui-component-lib/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,6 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() label: string = 'Button';
+  @Input() lib: 'material' | 'primeng' | 'bootstrap' = 'bootstrap';
+  @Input() color: 'success' | 'info' | 'warning' | 'danger' | 'help' | 'primary' | 'secondary' | 'contrast' | null | undefined = 'primary';
+  @Input() size: 'small' | 'medium' | 'large' | 'normal' = 'normal';
+  @Input() iconClass: string = '';
+  @Input() iconPosition: 'left' | 'right' = 'left';
+  @Input() fullWidth: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() rounded: boolean = false;
+  @Input() outlined: boolean = false;
+  severity: string = '';
 
+  ngOnInit() {
+    this.severity = this.color ? this.color : 'primary';
+  }
 }
