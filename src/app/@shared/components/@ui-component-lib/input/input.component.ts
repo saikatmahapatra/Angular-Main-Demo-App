30a810c1ba0d0f18dbe5739051135708148d7556
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-11-22 17:45:57 +0530

    Input modified

--- src/app/@shared/components/@ui-component-lib/input/input.component.ts
+++ src/app/@shared/components/@ui-component-lib/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, , Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -12,9 +12,18 @@ export class InputComponent implements OnInit {
 
   @Input() type: string = 'text';
   @Input() label: string = '';
+  @Input() name: string = '';
+  @Input() id: string = '';
   @Input() placeholder: string = '';
+  @Input() formControlName: string = '';  
   @Input() disabled: boolean = false
   @Input() value: string = '';
+  @Input() cssClass: string = '';
+  @Input() readonly: boolean = false;
+  @Input() required: boolean = false;
+
+  // PrimeNg specific inputs can be added here as needed
+
   @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
 
   onInput(event: Event): void {
