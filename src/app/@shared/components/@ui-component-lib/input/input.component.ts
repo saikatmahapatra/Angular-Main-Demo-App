46817c1c427044db80984cfb4dfb74bd3a9677fb
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-11-22 17:39:31 +0530

    basic input created

--- src/app/@shared/components/@ui-component-lib/input/input.component.ts
+++ src/app/@shared/components/@ui-component-lib/input/input.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, , Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   standalone: false,
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  styleUrls: ['./input.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
-export class InputComponent {
 
+export class InputComponent implements OnInit {
+
+  @Input() type: string = 'text';
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() disabled: boolean = false
+  @Input() value: string = '';
+  @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
+
+  onInput(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.valueChange.emit(inputElement.value);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
