0188ec914626add1dfca3a73178adc9c4267164f
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-11-29 14:58:07 +0530

    added dymmy menu

--- src/app/@shared/components/sidebar/sidebar.component.ts
+++ src/app/@shared/components/sidebar/sidebar.component.ts
@@ -1,123 +1,149 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/@core/services/auth.service';
-import { CommonService } from 'src/app/@core/services/common.service';
-import { NavigationService } from 'src/app/@core/services/navigation.service';
+import { MenuItem } from 'primeng/api';
 @Component({
-    selector: 'app-sidebar',
-    templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.scss'],
-    standalone: false
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  standalone: false
 })
-export class SidebarComponent implements OnInit {
-  // showSideNav: Observable<any> | undefined;
-  // @Input() sidenavTemplateRef: any;
-  // @Input() duration: number = 0.25;
-  // @Input() navWidth: number = window.innerWidth;
-  // @Input() direction: SideNavDirection = SideNavDirection.Left;
-  userRole = '';
-  loggedInUserId = '';
-  welcomeUserText = '';
-  sideBarMenuItems: any = [];
-  constructor(private navService: NavigationService, private commonSvc: CommonService, private authSvc: AuthService) { }
+export class SidebarComponent implements OnInit{
+  model: MenuItem[] = [];
 
-  ngOnInit(): void {
-    this.userRole = this.authSvc.getRoleId();
-    this.loggedInUserId = this.authSvc.getUserId();
-    const user = this.authSvc.getUser();
-    this.welcomeUserText = user.user_full_name;
-
-    this.sideBarMenuItems = [
+  ngOnInit() {
+    this.model = [
       {
-        id: 1,
-        label: "Getting Started",
-        icon: "home",
-        showIcon: false,
-        isAdminMenu: false,
+        label: 'Home',
+        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }]
+      },
+      {
+        label: 'UI Components',
         items: [
-          { id: 1, label: "Dashboard", icon: "home", showIcon: true, url: ["/dashboard"] },
-          { id: 2, label: "Analytics", icon: "dashboard", showIcon: true, url: ["/dashboard/my-analytics/emp/", this.loggedInUserId] }
+          { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
+          { label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input'] },
+          { label: 'Button', icon: 'pi pi-fw pi-mobile', class: 'rotated-icon', routerLink: ['/uikit/button'] },
+          { label: 'Table', icon: 'pi pi-fw pi-table', routerLink: ['/uikit/table'] },
+          { label: 'List', icon: 'pi pi-fw pi-list', routerLink: ['/uikit/list'] },
+          { label: 'Tree', icon: 'pi pi-fw pi-share-alt', routerLink: ['/uikit/tree'] },
+          { label: 'Panel', icon: 'pi pi-fw pi-tablet', routerLink: ['/uikit/panel'] },
+          { label: 'Overlay', icon: 'pi pi-fw pi-clone', routerLink: ['/uikit/overlay'] },
+          { label: 'Media', icon: 'pi pi-fw pi-image', routerLink: ['/uikit/media'] },
+          { label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'] },
+          { label: 'Message', icon: 'pi pi-fw pi-comment', routerLink: ['/uikit/message'] },
+          { label: 'File', icon: 'pi pi-fw pi-file', routerLink: ['/uikit/file'] },
+          { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/uikit/charts'] },
+          { label: 'Timeline', icon: 'pi pi-fw pi-calendar', routerLink: ['/uikit/timeline'] },
+          { label: 'Misc', icon: 'pi pi-fw pi-circle', routerLink: ['/uikit/misc'] }
         ]
       },
       {
-        id: 2,
-        label: "Admin Menu",
-        icon: "admin",
-        showIcon: false,
-        isAdminMenu: true,
+        label: 'Pages',
+        icon: 'pi pi-fw pi-briefcase',
+        routerLink: ['/pages'],
         items: [
-          { id: 1, label: "Manage Employees", icon: "users", showIcon: true,  url: ["/emp/manage"] },
-          { id: 2, label: "Manage Contents", icon: "note", showIcon: true,  url: ["/cms/manage-cms"] },
-          { id: 3, label: "Manage Holidays", icon: "calendar", showIcon: true,  url: ["/cms/holiday-management"] },
-          { id: 4, label: "Manage Leave Applications", icon: "leave", showIcon: true,  url: ["/leave/manage"] },
-          { id: 5, label: "View & Upload Leave Balance", icon: "currency", showIcon: true,  url: ["/leave/balance"] },
-          { id: 6, label: "Timesheet Report", icon: "report", showIcon: true,  url: ["/timesheet/report"] },
-          { id: 7, label: "Manage Projects", icon: "projects", showIcon: true,  url: ["/project/manage-project"] },
-          { id: 8, label: "Manage Tasks", icon: "tasks", showIcon: true,  url: ["/project/manage-tasks"] },
-          { id: 10, label: "Settings", icon: "settings", showIcon: true,  url: ["/settings"] }
+          {
+            label: 'Landing',
+            icon: 'pi pi-fw pi-globe',
+            routerLink: ['/landing']
+          },
+          {
+            label: 'Auth',
+            icon: 'pi pi-fw pi-user',
+            items: [
+              {
+                label: 'Login',
+                icon: 'pi pi-fw pi-sign-in',
+                routerLink: ['/auth/login']
+              },
+              {
+                label: 'Error',
+                icon: 'pi pi-fw pi-times-circle',
+                routerLink: ['/auth/error']
+              },
+              {
+                label: 'Access Denied',
+                icon: 'pi pi-fw pi-lock',
+                routerLink: ['/auth/access']
+              }
+            ]
+          },
+          {
+            label: 'Crud',
+            icon: 'pi pi-fw pi-pencil',
+            routerLink: ['/pages/crud']
+          },
+          {
+            label: 'Not Found',
+            icon: 'pi pi-fw pi-exclamation-circle',
+            routerLink: ['/pages/notfound']
+          },
+          {
+            label: 'Empty',
+            icon: 'pi pi-fw pi-circle-off',
+            routerLink: ['/pages/empty']
+          }
         ]
       },
       {
-        id: 3,
-        label: "My Tasks",
-        icon: "tasks",
-        showIcon: false,
-        isAdminMenu: false,
+        label: 'Hierarchy',
         items: [
-          { id: 1, label: "Manage Leave Applications", icon: "leave", showIcon: true,  url: ["/leave/requests-to-approve"] }
+          {
+            label: 'Submenu 1',
+            icon: 'pi pi-fw pi-bookmark',
+            items: [
+              {
+                label: 'Submenu 1.1',
+                icon: 'pi pi-fw pi-bookmark',
+                items: [
+                  { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+                  { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+                  { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
+                ]
+              },
+              {
+                label: 'Submenu 1.2',
+                icon: 'pi pi-fw pi-bookmark',
+                items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
+              }
+            ]
+          },
+          {
+            label: 'Submenu 2',
+            icon: 'pi pi-fw pi-bookmark',
+            items: [
+              {
+                label: 'Submenu 2.1',
+                icon: 'pi pi-fw pi-bookmark',
+                items: [
+                  { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+                  { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
+                ]
+              },
+              {
+                label: 'Submenu 2.2',
+                icon: 'pi pi-fw pi-bookmark',
+                items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
+              }
+            ]
+          }
         ]
       },
       {
-        id: 4,
-        label: "Self Services",
-        icon: "selfService",
-        showIcon: false,
-        isAdminMenu: false,
+        label: 'Get Started',
         items: [
-          { id: 1, label: "Log Timesheet", icon: "timesheet", showIcon: true,  url: ["/timesheet/log-work"] },
-          { id: 2, label: "Change Workflow Approvers", icon: "datacheck", showIcon: true,  url: ["/emp/change-approvers"] },
-          { id: 3, label: "Apply Leave", icon: "send", showIcon: true,  url: ["/leave/apply"] },
-          { id: 4, label: "Leave History", icon: "history2", showIcon: true,  url: ["/leave/history"] }
+          {
+            label: 'Documentation',
+            icon: 'pi pi-fw pi-book',
+            routerLink: ['/documentation']
+          },
+          {
+            label: 'View Source',
+            icon: 'pi pi-fw pi-github',
+            url: 'https://github.com/primefaces/sakai-ng',
+            target: '_blank'
+          }
         ]
-      },
-      {
-        id: 5,
-        label: "Organization",
-        icon: "globe",
-        showIcon: false,
-        isAdminMenu: false,
-        items: [
-          { id: 1, label: "Employees", icon: "users", showIcon: true,  url: ["/emp/view-employees"] },
-          { id: 2, label: "My Reportees", icon: "diversity", showIcon: true,  url: ["/emp/view-reportees"] },
-          { id: 3, label: "Holidays", icon: "calendar", showIcon: true,  url: ["/cms/holiday-calendar"] }
-        ]
-      },
-      {
-        id: 6,
-        label: "My Account",
-        icon: "user",
-        showIcon: false,
-        isAdminMenu: false,
-        items: [
-          { id: 1, label: "My Profile", icon: "user", showIcon: true, url: ["/emp/my-profile"] },
-          { id: 2, label: "Change Password", icon: "password", showIcon: true, url: ["/emp/change-password"] },
-          { id: 3, label: "Logout", icon: "signout", showIcon: true, url: ["/auth/logout"] }
-        ]
-      },
+      }
     ];
-  }
-
-  transform(value: string, size: number = 10): string {
-    if (!value) {
-      return '';
-    }
-    const limit = size > 0 ? size : 10;
-    return value.length > limit ? value.substring(0, limit) + '...' : value;
-  }
-
-  closeSideBar() {
-    if (this.commonSvc.getScreenResolutionBreakPoint() === 'xs' || this.commonSvc.getScreenResolutionBreakPoint() === 'sm' || this.commonSvc.getScreenResolutionBreakPoint() === 'md') {
-      this.navService.toggleNavState();
-    }
   }
 
 }
