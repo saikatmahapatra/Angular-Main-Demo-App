5368fdcd44fb390ce1ec69a1f207f6a3b56cc874
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-11-29 15:16:04 +0530

    Dark sitch works

--- src/app/@shared/components/header/header.component.ts
+++ src/app/@shared/components/header/header.component.ts
@@ -1,23 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { LayoutService } from '../../services/layout.service';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
-    standalone: false
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: false
 })
 export class HeaderComponent {
+  items!: MenuItem[];
 
   constructor(public layoutService: LayoutService) {
-
-  }
-
-  toggleMenu() {
-    
   }
 
   toggleDarkMode() {
-
+    this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
   }
 }
