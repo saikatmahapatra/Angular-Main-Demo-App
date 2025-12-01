5906fa060759cc168458a31556b1d1fda7998437
Saikat Mahapatra <saikat.mahapatra@accenture.com>
2025-12-01 18:24:22 +0530

    code cleanup

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthLayoutComponent } from './auth-layout.component';
 import { LoginFormComponent } from './login-form/login-form.component';
