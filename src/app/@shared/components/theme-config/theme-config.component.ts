7bae9b30d2f6ca8f1018ea319bc61912ae53e161
Saikat Mahapatra <saikat.mahapatra@accenture.com>
2025-12-01 15:26:01 +0530

    modify token

--- src/app/@shared/components/theme-config/theme-config.component.ts
+++ src/app/@shared/components/theme-config/theme-config.component.ts
@@ -1,4 +1,4 @@
-import {isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
 import { Component, computed, inject, PLATFORM_ID, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { $t, updatePreset, updateSurfacePalette } from '@primeuix/themes';
@@ -38,7 +38,10 @@ declare type SurfacesType = {
   selector: 'app-theme-config',
   templateUrl: './theme-config.component.html',
   styleUrls: ['./theme-config.component.scss'],
-  standalone: false
+  standalone: false,
+  host: {
+    class: 'hidden absolute top-13 right-0 w-72 p-4 bg-surface-0 dark:bg-surface-900 border border-surface rounded-border origin-top shadow-[0px_3px_5px_rgba(0,0,0,0.02),0px_0px_2px_rgba(0,0,0,0.05),0px_1px_4px_rgba(0,0,0,0.08)]'
+  }
 })
 export class ThemeConfigComponent {
   router = inject(Router);
