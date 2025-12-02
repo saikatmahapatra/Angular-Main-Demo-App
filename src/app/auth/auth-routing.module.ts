2df7dbd4fbce0ccfcf118c92d74bc7940e9bbcb8
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-12-02 11:32:49 +0530

    components organized into components

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginFormComponent } from './login-form/login-form.component';
-import { ForgotPasswordFormComponent } from './forgot-password-form/forgot-password-form.component';
-import { ResetPasswordFormComponent } from './reset-password-form/reset-password-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ForgotPasswordFormComponent } from './components/forgot-password-form/forgot-password-form.component';
+import { ResetPasswordFormComponent } from './components/reset-password-form/reset-password-form.component';
 import { AuthLayoutComponent } from './auth-layout.component';
-import { TwoFactorAuthComponent } from './login-form/two-factor-auth/two-factor-auth.component';
+import { TwoFactorAuthComponent } from './components/login-form/two-factor-auth/two-factor-auth.component';
 const routes: Routes = [
   {path: '', component: AuthLayoutComponent, children: [
     { path: '', component: LoginFormComponent},
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthLayoutComponent } from './auth-layout.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { ForgotPasswordFormComponent } from './forgot-password-form/forgot-password-form.component';
-import { ResetPasswordFormComponent } from './reset-password-form/reset-password-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ForgotPasswordFormComponent } from './components/forgot-password-form/forgot-password-form.component';
+import { ResetPasswordFormComponent } from './components/reset-password-form/reset-password-form.component';
 import { SharedModule } from '../@shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TwoFactorAuthComponent } from './login-form/two-factor-auth/two-factor-auth.component';
+import { TwoFactorAuthComponent } from './components/login-form/two-factor-auth/two-factor-auth.component';
 import { PrimeNgModule } from '../prime-ng.module';
 import { TranslateLangModule } from '../translate-language.module';
 
