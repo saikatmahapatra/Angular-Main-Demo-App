535032cb55d4ea6669e5ce8151a4f35da2925c47
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2025-12-02 11:42:04 +0530

    add dashboard landing page

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardLayoutComponent } from './dashboard-layout.component';
 import { PostDetailsComponent } from './components/post-details/post-details.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { DashboardLandingPageComponent } from './components/dashboard-landing-page/dashboard-landing-page.component';
 const routes: Routes = [
   {
     path: '', component: DashboardLayoutComponent, children: [
+      { path: '', component: DashboardLandingPageComponent },
       { path: 'post-details/:id', component: PostDetailsComponent },
       { path: 'analytics/:entity/:entityId', component: AnalyticsComponent },
       { path: 'my-analytics/:entity/:entityId', component: AnalyticsComponent }]
