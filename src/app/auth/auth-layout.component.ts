f809784df38a199df77696e4dac66b447edf2fb8
Saikat Mahapatra <saikat.mahapatra@accenture.com>
2025-12-03 19:23:54 +0530

    product name add

--- src/app/auth/auth-layout.component.ts
+++ src/app/auth/auth-layout.component.ts
@@ -3,30 +3,15 @@ import { MyAppConfig } from 'src/app/app.config';
 import { CommonService } from '../@core/services/common.service';
 
 @Component({
-    selector: 'app-auth-layout',
-    templateUrl: './auth-layout.component.html',
-    styleUrls: ['./auth-layout.component.scss'],
-    standalone: false
+  selector: 'app-auth-layout',
+  templateUrl: './auth-layout.component.html',
+  styleUrls: ['./auth-layout.component.scss'],
+  standalone: false
 })
-export class AuthLayoutComponent implements OnInit {
-  imgPath = '@assets/img/7.svg';
-  constructor(private commonSvc: CommonService) { 
+export class AuthLayoutComponent {
+  productName = MyAppConfig.productName;
+  constructor(private commonSvc: CommonService) {
     this.commonSvc.setTitle('Auth');
-    
-  }
-
-  ngOnInit(): void {
-    if(localStorage.getItem('theme') === 'dark') {
-      this.imgPath = '@assets/img/7-dark.svg';
-    }
-  }
-
-  getThemeName(themeName: string) {
-    if(themeName === 'dark') {
-      this.imgPath = '@assets/img/7-dark.svg';
-    } else {
-      this.imgPath = '@assets/img/7.svg';
-    }
   }
 
 }
--- src/app/web-page/components/footer-widget/footer-widget.component.ts
+++ src/app/web-page/components/footer-widget/footer-widget.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MyAppConfig } from 'src/app/app.config';
 
 @Component({
   selector: 'app-footer-widget',
@@ -8,5 +9,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./footer-widget.component.scss']
 })
 export class FooterWidgetComponent {
+  productName = MyAppConfig.productName;
   constructor(public router: Router) {}
 }
