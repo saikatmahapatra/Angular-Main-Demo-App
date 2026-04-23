8943b61cda10ce7019fd3fbd9686274b9eb2b3c8
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2026-04-23 10:06:45 +0530

    Create Wrapper Pattern

---
+++ src/app/@app-ui-lib/app-ui-lib.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppUiLibRoutingModule } from './app-ui-lib-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppUiLibRoutingModule
+  ]
+})
+export class AppUiLibModule { }
