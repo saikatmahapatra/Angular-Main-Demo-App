b73fe5a3b6a91266a187ec8580414a588ef91232
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2026-04-23 10:33:42 +0530

    export button

--- src/app/@app-ui-lib/index.ts
+++ src/app/@app-ui-lib/index.ts
@@ -1,9 +1,31 @@
 import { ButtonComponent } from "./button/button.component";
 import { ChartsComponent } from "./charts/charts.component";
 import { IconComponent } from "./icon/icon.component";
+import { DataTableComponent } from "./data-table/data-table.component";
+import { InputComponent } from "./input/input.component";
+import { FileUploadComponent } from "./file-upload/file-upload.component";
+import { RadioComponent } from "./radio/radio.component";
+import { SelectComponent } from "./select/select.component";
 
-export {
+
+// Group them for easy legacy importing
+export const APP_UI_COMPONENTS = [
   ButtonComponent,
   ChartsComponent,
-  IconComponent
-};
+  IconComponent,
+  DataTableComponent,
+  InputComponent,
+  FileUploadComponent,
+  RadioComponent,
+  SelectComponent
+] as const;
+
+// Export them individually for tree-shaking and modern imports
+export * from "./button/button.component";
+export * from "./charts/charts.component";
+export * from "./icon/icon.component";
+export * from "./data-table/data-table.component";
+export * from "./input/input.component";
+export * from "./file-upload/file-upload.component";
+export * from "./radio/radio.component";
+export * from "./select/select.component";
