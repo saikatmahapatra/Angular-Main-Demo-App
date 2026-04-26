bc692cc3f07767b85c6ccca3335a60758b3e325a
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2026-04-26 08:57:12 +0530

    add new components, rename, delete

---
+++ src/app/@ui-kit/data-grid/data-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-grid',
+  standalone: true,
+  template: `
+    <p>
+      data-grid works!
+    </p>
+  `,
+  styles: ``,
+})
+export class DataGridComponent {
+
+}
