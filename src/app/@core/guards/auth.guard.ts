df30c572a722ecdc9d4c4d0f69edbea503c966fa
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2026-08-02 20:04:40 +0530

    Refactor using custom AlertMessageService

--- src/app/@core/guards/auth.guard.ts
+++ src/app/@core/guards/auth.guard.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { AlertService } from '@core/services/alert.service';
+import { AlertMessageService } from '@core/services/alert-message.service';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard  {
-  constructor(private router: Router, private authService: AuthService, private alertService: AlertService) { }
+  constructor(
+    private readonly router: Router, 
+    private readonly authService: AuthService, 
+    private readonly alertMessageService: AlertMessageService
+  ) { }
   canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     let routeMessage: string = '';
     const isTokenExpired = this.authService.isTokenExpired();
 
     this.authService.validateToken().subscribe({
       error: (error: HttpErrorResponse) => {
-        this.alertService.setAlert('info', 'JWT Token either expired or not matched. Please login to continue.', true);
+        this.alertMessageService.setAlert('info', 'JWT Token either expired or not matched. Please login to continue.', true);
         this.authService.clearStorageData();
       }
     });
@@ -32,7 +36,7 @@ export class AuthGuard  {
       routeMessage = "Your session has expired."
     }
 
-    if (routeMessage) this.alertService.setAlert('error', routeMessage, false);
+    if (routeMessage) this.alertMessageService.setAlert('error', routeMessage, false);
     //console.log(routeMessage);
 
     this.router.navigate(['/auth/login'], { queryParams: { returnUrl: state.url } });
