c5d068e76201c7984256cdf7c0b49809c892086b
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2026-08-02 20:08:22 +0530

    Refactor using custom AlertMessageService

--- src/app/@core/interceptors/auth.interceptor.ts
+++ src/app/@core/interceptors/auth.interceptor.ts
@@ -4,19 +4,23 @@ import { finalize, Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { LoaderService } from '../services/loader.service';
 import { MyAppConfig } from 'src/app/app.config';
-import { AlertService } from '../services/alert.service';
+import { AlertMessageService } from '@core/services/alert-message.service';
 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authSvc: AuthService, private loader: LoaderService, private alertSvc: AlertService) {}
+  constructor(
+    private readonly authSvc: AuthService, 
+    private readonly loader: LoaderService, 
+    private readonly alertMessageSvc: AlertMessageService
+  ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const authToken = this.authSvc.getToken();
     const isMockServer = MyAppConfig.useMockServer;
     this.loader.show();
-    //this.alertSvc.clear();
+    //this.alertMessageSvc.clear();
     if(authToken && !isMockServer) {
       const clonedReq = request.clone({
         setHeaders: {
